450a8a84603b35aef9a75030e132fc828299b29a
Sven Baumann <baumann.sv@gmail.com>
2015-10-27 13:36:26 +0100

    configure the event subscribers

---
+++ contao/config/event_subscribers.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * FRAMEWORK
+ *
+ * Copyright (C) FRAMEWORK
+ *
+ * @package   brugg-regio-ch
+ * @file      event_subscribers.php
+ * @author    Sven Baumann <baumann.sv@gmail.com>
+ * @author    Dominik Tomasi <dominik.tomasi@gmail.com>
+ * @license   GNU/LGPL
+ * @copyright Copyright 2015 owner
+ */
+
+return array
+(
+    new \ContaoBlackForest\Modify\Search\Content\News\Subscribers\ModifySearchContent(),
+);
